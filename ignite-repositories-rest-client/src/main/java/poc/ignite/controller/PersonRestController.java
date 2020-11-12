fd8e729640b3547e34bae0e31bfae27784451bab
ashishb888 <ashish.bhosle008@gmail.com>
2020-11-12 18:09:04 +0530

    ignite-repositories-rest-clientr: All the CRUD via IgniteRepository

--- ignite-repositories-rest-client/src/main/java/poc/ignite/controller/PersonRestController.java
+++ ignite-repositories-rest-client/src/main/java/poc/ignite/controller/PersonRestController.java
@@ -1,7 +1,11 @@
 package poc.ignite.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,5 +22,22 @@ public class PersonRestController {
 	@GetMapping
 	public Iterable<Person> getAllPersons() {
 		return ps.findAll();
+	}
+
+	@PostMapping
+	public Person saveAPerson(@RequestBody Person person) {
+		return ps.save(person.getId(), person);
+	}
+
+	@DeleteMapping
+	public String deletePersons(@RequestBody Iterable<Integer> ids) {
+		ps.deleteAll(ids);
+
+		return "Deleted!";
+	}
+
+	@PutMapping
+	public Person updateAPerson(@RequestBody Person person) {
+		return ps.save(person.getId(), person);
 	}
 }
